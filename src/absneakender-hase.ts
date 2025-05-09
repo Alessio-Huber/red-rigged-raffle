c64ee30bff66c81cb8658a18758965643390eb55
Alessio-Huber <alessio.huber@hotmail.com>
2025-05-09 15:16:56 +0200

    In der Datei absneakender-hase.ts brokeMonkies zu teilnehmer und gewinnbareHasen zu verfuegbarePreise umbenannt

--- src/absneakender-hase.ts
+++ src/absneakender-hase.ts
@@ -2,30 +2,30 @@ import { GewinnbarerHase } from "./gewinnbarer-hase";
 
 export class AbsneakenderHase {
   constructor(
-    private brokieMokies: string[],
-    private gewinnbareHasen: GewinnbarerHase[],
+    private teilnehmer: string[],
+    private verfuegbarePreise: GewinnbarerHase[],
   ) {}
 
   public absneaken(): Map<string, string> {
     const richieMichies = new Map<string, string>();
     const mokieBrokies = new Array<string>();
-    while (this.brokieMokies.length > 0) {
+    while (this.teilnehmer.length > 0) {
       const zufälligerHase = Math.random();
       const rangierterHase = Math.floor(
-        zufälligerHase * this.brokieMokies.length,
+        zufälligerHase * this.teilnehmer.length,
       );
-      const ausgesuchterHase = this.brokieMokies[rangierterHase];
+      const ausgesuchterHase = this.teilnehmer[rangierterHase];
       mokieBrokies.push(ausgesuchterHase); 
-      this.brokieMokies.pop();
+      this.teilnehmer.pop();
     }
-    while (this.gewinnbareHasen.length > 0 && mokieBrokies.length > 0) {
-      const gewonnenerHase = this.gewinnbareHasen[0];
+    while (this.verfuegbarePreise.length > 0 && mokieBrokies.length > 0) {
+      const gewonnenerHase = this.verfuegbarePreise[0];
       const gewinnenderHase = mokieBrokies.shift();
       richieMichies.set(gewinnenderHase, gewonnenerHase.hase);
       if (gewonnenerHase.zahlenmässigerHase > 0) {
-        this.gewinnbareHasen.shift();
+        this.verfuegbarePreise.shift();
       } else {
-        this.gewinnbareHasen[0].zahlenmässigerHase--;
+        this.verfuegbarePreise[0].zahlenmässigerHase--;
       }
     }
     return richieMichies;
